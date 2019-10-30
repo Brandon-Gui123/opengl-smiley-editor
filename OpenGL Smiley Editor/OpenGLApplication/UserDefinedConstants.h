13d1602bb2610f379a5415279f6fa01b3e7676ea
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2019-10-31 00:15:36 +0800

    Added header file for user-defined constants
    This header file will contain constants we have defined for our program, such as epsilon.

---
+++ OpenGL Smiley Editor/OpenGLApplication/UserDefinedConstants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef USER_DEFINED_CONSTANTS_H
+#define USER_DEFINED_CONSTANTS_H
+
+/// <summary>An extremely small value used to check if two float values are "close enough" to be considered equal.</summary>
+#define USER_DEFINED_MATH_EPSILON 1e-8
+
+#endif
