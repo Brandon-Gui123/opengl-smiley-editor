6f902a598873780ad1d670d96cb15fbfb8a2eb5b
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2019-10-31 00:21:10 +0800

    Added equality check for floats (not really).
    This method doesn't check if two floats are exactly equal, but rather, checks if they are "close enough" to be equal.
    The value of "close enough" is known as epsilon.
    
    This method makes use of Donald Knuth's algorithm on relative epsilon. If the floats you are comparing are huge, say 1,000,000, the epsilon will be small, say 0.01. But if the floats you are comparing are small, say 0.00001, the epsilon will be tiny, say 0.000000001. However, if both floats are very close to zero, then the absolute epsilon would be used instead.

--- OpenGL Smiley Editor/OpenGLApplication/BrandonUtils.cpp
+++ OpenGL Smiley Editor/OpenGLApplication/BrandonUtils.cpp
@@ -1,4 +1,10 @@
 #include "BrandonUtils.h"
+
+// for user-defined mathematical constants, like epsilon
+#include "UserDefinedConstants.h"
+
+#include <cmath>    // for fabs(), which returns the positive value for a given number
+
 float BrandonUtils::map(float current, float oldMin, float oldMax, float newMin, float newMax)
 {
     // TODO: Overflow check?
@@ -20,4 +26,29 @@ float BrandonUtils::map(float current, float oldMin, float oldMax, float newMin,
 
     // the following statement combines all of the above into one expression
     return (((current - oldMin) / (oldMax - oldMin)) * (newMax - newMin)) + newMin;
+}
+
+bool BrandonUtils::isApproximatelyEqual(float first, float second)
+{
+    // since relative epsilon comparison fails when numbers approach closer to zero,
+    // we will first check using an absolute epsilon
+    if (fabs(first - second) <= USER_DEFINED_MATH_EPSILON)
+    {
+        return true;
+    }
+
+    // otherwise, fallback to Knuth's algorithm for relative epsilon
+
+    // select the larger value among the two floats
+    float largerValue = (fabs(first) < fabs(second)) ? fabs(second) : fabs(first);
+
+    // calculate the relative epsilon because the absolute epsilon is too big for 
+    // extremely small values (values smaller than the epsilon) and too small for
+    // huge values (values way way larger than the epsilon, like 10,000, making 10,000.001 not equal to 10,000)
+    // so we need to pick a good epsilon for comparison
+    // here, the epsilon is a percentage, and we use it to ask if
+    // the smaller float is within the "epsilon" percentage of the larger float
+    float relativeEpsilon = largerValue * USER_DEFINED_MATH_EPSILON;
+
+    return fabs(first - second) <= relativeEpsilon;
 }
--- OpenGL Smiley Editor/OpenGLApplication/BrandonUtils.h
+++ OpenGL Smiley Editor/OpenGLApplication/BrandonUtils.h
@@ -11,6 +11,13 @@ namespace BrandonUtils
 {
     /// <summary>Converts a number from an old range to a new range.</summary>
     float map(float current, float oldMin, float oldMax, float newMin, float newMax);
+
+    /// <summary>
+    /// Checks if both floats are "close enough" to be equal. The value of "close enough" depends on the value of the floats.
+    /// If the floats are huge, "close enough" will be big. And if the floats are small, "close enough" will be very small.
+    /// This is Donald Knuth's algorithm on relative epsilons.
+    /// </summary>
+    bool isApproximatelyEqual(float first, float second);
 }
 
 #endif
