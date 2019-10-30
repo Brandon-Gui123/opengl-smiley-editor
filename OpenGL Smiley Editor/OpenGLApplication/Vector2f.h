06be25e7ccf7ea70d8252c119965b034d61b5eaf
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2019-10-30 21:47:10 +0800

    Unfriended overloaded binary operators.
    Moved the overloaded binary friend functions and removed the friend modifier off those overloaded functions.
    
    This is because the member variables in the Vector3f class are already public and so we don't need additional privileges for these functions; they can already access those methods.

--- OpenGL Smiley Editor/OpenGLApplication/Vector2f.h
+++ OpenGL Smiley Editor/OpenGLApplication/Vector2f.h
@@ -19,23 +19,6 @@ public:
     /// <summary>Constructs a Vector2f with the specified parameters for the x and y values.</summary>
     Vector2f(float x, float y);
     
-    // We need to make operators a friend function to access internal variables and to place it as a
-    // forward declaration 
-    /// <summary>Returns a vector that has the sum of the x and y values from the provided 2 vectors</summary>
-    friend Vector2f operator+(const Vector2f &first, const Vector2f &second);
-
-    /// <summary>Returns a vector that has the difference between the x and y values from the provided 2 vectors</summary>
-    friend Vector2f operator-(const Vector2f &first, const Vector2f &second);
-
-    /// <summary>Returns a vector that has all of its values multiplied by the specified multiplier</summary>
-    friend Vector2f operator*(const Vector2f &vector, float multiplier);
-
-    /// <summary>Returns a vector that has all of its values multiplied by the specified multiplier</summary>
-    friend Vector2f operator*(float multiplier, const Vector2f &vector);
-
-    /// <summary>Returns a vector that has all of its values divided by the specified multiplier</summary>
-    friend Vector2f operator/(const Vector2f &vector, float divisor);
-
     // This operator is overloaded using a member function
     // Because this is a member function, you have access to "this".
     // The return type can be void, but if we did it that way, we can no longer chain operators (e.g. a + b += c)
@@ -51,5 +34,20 @@ public:
     /// <summary>Divides the vector on the left of this operand with the float on the right.</summary>
     Vector2f operator/=(float divisor);
 };
+
+/// <summary>Returns a vector that has the sum of the x and y values from the provided 2 vectors</summary>
+Vector2f operator+(const Vector2f &first, const Vector2f &second);
+
+/// <summary>Returns a vector that has the difference between the x and y values from the provided 2 vectors</summary>
+Vector2f operator-(const Vector2f &first, const Vector2f &second);
+
+/// <summary>Returns a vector that has all of its values multiplied by the specified multiplier</summary>
+Vector2f operator*(const Vector2f &vector, float multiplier);
+
+/// <summary>Returns a vector that has all of its values multiplied by the specified multiplier</summary>
+Vector2f operator*(float multiplier, const Vector2f &vector);
+
+/// <summary>Returns a vector that has all of its values divided by the specified multiplier</summary>
+Vector2f operator/(const Vector2f &vector, float divisor);
 
 #endif
