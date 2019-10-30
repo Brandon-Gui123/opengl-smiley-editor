2759c787c3b659c9e644e0da576e33b47e5a31c2
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2019-10-30 11:02:57 +0800

    Added project and setup for OpenGL.
    Added the project and set it up to support our OpenGL needs for IGRA.

---
+++ OpenGL Smiley Editor/OpenGLApplication/OpenGLApplication.cpp
@@ -0,0 +1,329 @@
+// OpenGLApplication.cpp : Defines the entry point for the application.
+//
+
+#include "framework.h"
+#include "OpenGLApplication.h"
+
+#include <gl/GL.h>  // OpenGL 32-bit library
+#include <gl/GLU.h> // GLU 32-bit library
+
+#define MAX_LOADSTRING 100
+
+// defines for extra width and height due to window toolbar and border
+#define EXTRA_WIDTH 16
+#define EXTRA_HEIGHT 60
+
+// Global Variables:
+int width = 400;
+int height = 400;
+GLuint PixelFormat;                     // Type is an OpenGL pre-defined unsigned int. These types ensure cross-platform compatibility.
+HDC hDC = NULL;                         // A handle to the device context
+HWND hWnd = NULL;                       // A handle to the window
+HGLRC hRC = NULL;                       // A handle to an OpenGL Rendering Context
+HINSTANCE hInst;                        // current instance
+WCHAR szTitle[MAX_LOADSTRING];          // The title bar text
+WCHAR szWindowClass[MAX_LOADSTRING];    // the main window class name
+
+// Pixel Format Descriptor
+static PIXELFORMATDESCRIPTOR pfd
+{
+    sizeof(PIXELFORMATDESCRIPTOR),
+    1,                      // version number
+    PFD_DRAW_TO_WINDOW |    // format must support window
+    PFD_SUPPORT_OPENGL |    // format must support OpenGL
+    PFD_DOUBLEBUFFER,       // must support double buffering
+    PFD_TYPE_RGBA,          // request an RGBA format
+    16,                     // select our colour depth, which will be 16
+    0, 0, 0, 0, 0, 0,       // colour bits ignored
+    0,                      // no alpha buffer
+    0,                      // shift bit ignored
+    0,                      // no accumulation buffer
+    0, 0, 0, 0,             // accumulation bits ignored
+    32,                     // 32-bit Z-Buffer (depth buffer)
+    0,                      // no stencil buffer
+    0,                      // no auxiliary buffer
+    PFD_MAIN_PLANE,         // main drawing layer
+    0,                      // reserved
+    0, 0, 0                 // layer masks ignored
+};
+
+// Forward declarations created by this developer:
+int InitOpenGL();
+void ReSizeGLScene(GLsizei width, GLsizei height);
+void DrawGLScene();
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Place code here.
+
+    // Initialize global strings
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_OPENGLAPPLICATION, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Perform application initialization:
+    if (!InitInstance (hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_OPENGLAPPLICATION));
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+
+            DrawGLScene();
+            SwapBuffers(hDC);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+
+
+//
+//  FUNCTION: MyRegisterClass()
+//
+//  PURPOSE: Registers the window class.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_OPENGLAPPLICATION));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_OPENGLAPPLICATION);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCTION: InitInstance(HINSTANCE, int)
+//
+//   PURPOSE: Saves instance handle and creates main window
+//
+//   COMMENTS:
+//
+//        In this function, we save the instance handle in a global variable and
+//        create and display the main program window.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Store instance handle in our global variable
+
+   hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, 0, 0, width + EXTRA_WIDTH, height + EXTRA_HEIGHT, nullptr, nullptr, hInstance, nullptr);
+
+   InitOpenGL();
+
+   if (!hWnd)
+   {
+      return FALSE;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   ReSizeGLScene(width, height);
+
+   return TRUE;
+}
+
+//
+//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PURPOSE: Processes messages for the main window.
+//
+//  WM_COMMAND  - process the application menu
+//  WM_PAINT    - Paint the main window
+//  WM_DESTROY  - post a quit message and return
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_COMMAND:
+        {
+            int wmId = LOWORD(wParam);
+            // Parse the menu selections:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Add any drawing code that uses hdc here...
+            EndPaint(hWnd, &ps);
+        }
+        break;
+    case WM_SIZE:
+        {
+        ReSizeGLScene(LOWORD(lParam), LOWORD(lParam));
+        break;
+        }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+
+// Message handler for about box.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+int InitOpenGL()
+{
+    // get device context
+    if (!(hDC = GetDC(hWnd)))
+    {
+        MessageBox(
+            NULL,
+            L"Can't create a GL device context!",
+            L"ERROR",
+            MB_OK | MB_ICONEXCLAMATION
+        );
+        return 0;
+    }
+
+    // check if Windows can find a matching pixel format
+    if (!(PixelFormat = ChoosePixelFormat(hDC, &pfd)))
+    {
+        MessageBox(
+            NULL,
+            L"Can't find a suitable pixel format!",
+            L"ERROR",
+            MB_OK | MB_ICONEXCLAMATION
+        );
+        return 0;
+    }
+
+    // try to set the pixel format
+    if (!SetPixelFormat(hDC, PixelFormat, &pfd))
+    {
+        MessageBox(
+            NULL,
+            L"Can't create a GL rendering context!",
+            L"ERROR",
+            MB_OK | MB_ICONEXCLAMATION
+        );
+        return 0;
+    }
+
+    // get a rendering context
+    if (!(hRC = wglCreateContext(hDC)))
+    {
+        MessageBox(
+            NULL,
+            L"Can't set the pixel format!",
+            L"ERROR",
+            MB_OK | MB_ICONEXCLAMATION
+        );
+        return 0;
+    }
+
+    // activate rendering context
+    if (!wglMakeCurrent(hDC, hRC))
+    {
+        MessageBox(
+            NULL,
+            L"Can't activate the GL rendering context!",
+            L"ERROR",
+            MB_OK | MB_ICONEXCLAMATION
+        );
+        return 0;
+    }
+
+    // for solving the depth-buffer issue
+    glClearDepth(1.0f);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
+
+    return 1;
+}
+
+void ReSizeGLScene(GLsizei width, GLsizei height)
+{
+    // prevent a division by zero error by setting the height to 1
+    if (height == 0)
+        height = 1;
+
+    // reset the current viewport
+    glViewport(0, 0, width, height);
+
+    // select the projection matrix
+    glMatrixMode(GL_PROJECTION);
+
+    // reset the projection matrix
+    glLoadIdentity();
+
+    // calculate the aspect ratio of the window
+    gluOrtho2D(-1.0, 1.0, -1.0, 1.0);
+}
+
+void DrawGLScene()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClearColor(0, 0, 0.8f, 1);
+}
