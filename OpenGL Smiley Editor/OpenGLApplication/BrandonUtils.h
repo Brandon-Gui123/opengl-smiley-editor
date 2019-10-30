2483fd54219218cff7a66e24a9e072ddb5cacac5
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2019-10-31 00:04:57 +0800

    Added BrandonUtils namespace.

---
+++ OpenGL Smiley Editor/OpenGLApplication/BrandonUtils.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef BRANDON_UTILS_H
+#define BRANDON_UTILS_H
+
+/// <summary>
+/// A namespace containing commonly-used functionality in this program, such as converting from
+/// OpenGL coordinates to Windows coordinates.
+/// </summary>
+namespace BrandonUtils
+{
+}
+
+#endif
