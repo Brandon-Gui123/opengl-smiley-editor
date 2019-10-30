97ca7514b5b8b9fec840c9808e538979d2cb5e3f
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2019-10-30 21:24:58 +0800

    Added overloads for operators +, -, * and /
    Added overloads for the binary operators: +, -, * and /

--- OpenGL Smiley Editor/OpenGLApplication/Vector2f.cpp
+++ OpenGL Smiley Editor/OpenGLApplication/Vector2f.cpp
@@ -5,3 +5,27 @@ Vector2f::Vector2f() : x(0.f), y(0.f)
 
 Vector2f::Vector2f(float x, float y) : x(x), y(y)
 {}
+Vector2f operator+(const Vector2f &first, const Vector2f &second)
+{
+    return Vector2f(first.x + second.x, first.y + second.y);
+}
+
+Vector2f operator-(const Vector2f & first, const Vector2f & second)
+{
+    return Vector2f(first.x - second.x, first.y - second.y);
+}
+
+Vector2f operator*(const Vector2f & vector, float multiplier)
+{
+    return Vector2f(vector.x * multiplier, vector.y * multiplier);
+}
+
+Vector2f operator*(float multiplier, const Vector2f & vector)
+{
+    return Vector2f(multiplier * vector.x, multiplier * vector.y);
+}
+
+Vector2f operator/(const Vector2f & vector, float divider)
+{
+    return Vector2f(vector.x / divider, vector.y / divider);
+}
--- OpenGL Smiley Editor/OpenGLApplication/Vector2f.h
+++ OpenGL Smiley Editor/OpenGLApplication/Vector2f.h
@@ -19,4 +19,20 @@ public:
     /// <summary>Constructs a Vector2f with the specified parameters for the x and y values.</summary>
     Vector2f(float x, float y);
     
+    // We need to make operators a friend function to access internal variables and to place it as a
+    // forward declaration 
+    /// <summary>Returns a vector that has the sum of the x and y values from the provided 2 vectors</summary>
+    friend Vector2f operator+(const Vector2f &first, const Vector2f &second);
+
+    /// <summary>Returns a vector that has the difference between the x and y values from the provided 2 vectors</summary>
+    friend Vector2f operator-(const Vector2f &first, const Vector2f &second);
+
+    /// <summary>Returns a vector that has all of its values multiplied by the specified multiplier</summary>
+    friend Vector2f operator*(const Vector2f &vector, float multiplier);
+
+    /// <summary>Returns a vector that has all of its values multiplied by the specified multiplier</summary>
+    friend Vector2f operator*(float multiplier, const Vector2f &vector);
+
+    /// <summary>Returns a vector that has all of its values divided by the specified multiplier</summary>
+    friend Vector2f operator/(const Vector2f &vector, float divisor);
 #endif
