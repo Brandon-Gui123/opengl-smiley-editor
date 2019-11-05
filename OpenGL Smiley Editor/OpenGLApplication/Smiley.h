27af15fe1715e2048f810423168d2c569245feb9
Brandon_Gui123 <BrandonGui123Alt@gmail.com>
2019-11-05 18:01:27 +0800

    Increased default resolution for circle and arc
    Increased the value for the resolution default parameter.

--- OpenGL Smiley Editor/OpenGLApplication/Smiley.h
+++ OpenGL Smiley Editor/OpenGLApplication/Smiley.h
@@ -36,13 +36,13 @@ private:
     // endAngle - the angle to end the arc
     // arcColour - the colour of the arc
     // resolution - determines the accuracy of the arc. Higher resolution results in rounder arcs
-    void DrawArc(Vector2f position, float radius, float startAngle, float endAngle, Color3f arcColor, int resolution = 20);
+    void DrawArc(Vector2f position, float radius, float startAngle, float endAngle, Color3f arcColor, int resolution = 32);
 
     /// <summary>
     /// Draws a circle at the specified position with a given radius and circle colour.
     /// The resolution of the circle changes how round the circle will appear.
     /// </summary>
-    void DrawCircle(Vector2f position, float radius, Color3f circleColor, int resolution = 20);
+    void DrawCircle(Vector2f position, float radius, Color3f circleColor, int resolution = 64);
 
     /// <summary> Draws the eyes of the smiley at the position, with a specified distance apart.</summary>
     void DrawEyes(Vector2f position, float eyeRadius, float distanceApart, Color3f eyeColor);
